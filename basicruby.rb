65d8eeb35c55fdb0693a15465df2e831bc8dadd7
John LaGreco <johnlagreco@gmail.com>
2015-12-19 16:33:48 -0500

    First function

---
+++ basicruby.rb
@@ -0,0 +1,25 @@
+# Create a function that takes a string and 
+# adds the phrase "Only in America!" to the end of it
+
+# Create a function to find the maximum value in 
+# an array of numbers. 
+# For instance: [100,10,-1000] should return 100.
+
+# Create a function that takes two 
+# arguments - both of them arrays. 
+# Inside of the function, combine the arrays using 
+# the items from the first array as keys and the 
+# second array as values into a hash. 
+
+# For example, when these two arrays are 
+# supplied as arguments:
+# ruby [:toyota, :tesla] ["Prius", "Model S"]
+# they should return a hash like so:
+# ruby {toyota: "Prius", tesla: "Model S"}
+
+
+def notsoFun(x)
+	x + "Only in America!"
+end
+puts notsoFun("Trump is the nominee? ")
+
