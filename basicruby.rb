e664f6d4155618fecb4b4eefc9a084b7b339f490
John LaGreco <johnlagreco@gmail.com>
2015-12-21 20:09:02 -0500

    Cleaned up file and finished.

--- basicruby.rb
+++ basicruby.rb
@@ -1,20 +1,3 @@
-# Create a function to find the maximum value in 
-# an array of numbers. 
-# For instance: [100,10,-1000] should return 100.
-
-# Create a function that takes two 
-# arguments - both of them arrays. 
-# Inside of the function, combine the arrays using 
-# the items from the first array as keys and the 
-# second array as values into a hash. 
-
-# For example, when these two arrays are 
-# supplied as arguments:
-# ruby [:toyota, :tesla] ["Prius", "Model S"]
-# they should return a hash like so:
-# ruby {toyota: "Prius", tesla: "Model S"}
-
-
 def itsFun(x)
 	x + "Only in America!"
 end
